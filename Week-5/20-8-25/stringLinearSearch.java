9a752d6d786c0d9370634c31f2050064383a9ce8
sohailqwerty7 <sohailqwerty7@gmail.com>
2025-08-20 05:56:59 +0000

    dsd

--- Week-5/20-8-25/stringLinearSearch.java
+++ Week-5/20-8-25/stringLinearSearch.java
@@ -5,11 +5,16 @@ public class stringLinearSearch {
                 return i;
             }
         }
-
         return -1;
     }
 
     public static void main(String[] args) {
-
+        String[] fruits = { "apple", "apple", "mango", "banana", "apple", "kiwi" };
+        int idx = temp(fruits, "kiwi");
+        if (idx == -1) {
+            System.out.println("Fruit not found");
+        } else {
+            System.out.println("Fruit found at " + idx);
+        }
     }
 }
