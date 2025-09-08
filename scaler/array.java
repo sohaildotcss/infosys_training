eafe6b213323ef8fd3df46f76d663efccbf3b4f2
sohailqwerty7 <sohailqwerty7@gmail.com>
2025-09-08 07:24:07 +0000

    scaler array

---
+++ scaler/array.java
@@ -0,0 +1,17 @@
+public class java {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        arr[0] = 1;
+        arr[5] = 3;
+        System.out.println(arr[9]);
+        System.out.println(arr);
+
+        float[] marks = new float[5];
+        System.out.println(marks[0]);
+
+        String[] names = new String[5];
+        System.out.println(names[0]);
+
+        
+    }
+}
